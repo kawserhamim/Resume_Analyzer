d48ea699bfe62b6bce41511b1b3bd4f46a95cda9
Kawser Hamim <kawser.hamim.456@gmail.com>
2026-06-29 21:57:17 +0600

    push

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://resume-analyzer-two-mocha.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
